8e1c330d07dd2f6f7981157d2568864c0eac4a93
Tom <tom.mclaughlin@gmail.com>
2015-12-05 21:12:05 -0600

    favicons

--- worried-war/src/main/java/be/shouldyou/Constants.java
+++ worried-war/src/main/java/be/shouldyou/Constants.java
@@ -4,13 +4,9 @@ package be.shouldyou;
  * Contains the client IDs and scopes for allowed clients consuming your API.
  */
 public class Constants {
-//    public static final String SERVICE_CLIENT_ID = "217649533837-31rcg1ev6gf52sp3r06l0nd1es4lp2kh.apps.googleusercontent.com";
 	/** 
-	 * This will be pulled from the JSON  file generated in the Google 
-	 * Developer's Console.  Because these are provided at runtime, any
-	 * changes here will require the application to be redeployed.  In 
-	 * our case, the service client is really just a Java VM running the
-	 * Android stub of the application.
-	 */
+	 * This should be the ID of a Service Client from the Google Developer's Console.
+	 * Do NOT use the email address here. 
+	 * */
 	public static final String SERVICE_CLIENT_ID = "106588608302127580477";
 }
